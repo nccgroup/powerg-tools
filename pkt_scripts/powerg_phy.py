422e3e8498640893b1c71e645023f6a64b540d32
James Chambers <jameschambers2@gmail.com>
2026-03-26 11:36:17 -0400

    function to convert base freq config value from modem

--- pkt_scripts/powerg_phy.py
+++ pkt_scripts/powerg_phy.py
@@ -12,3 +12,16 @@ OVERALL_CENTER_FREQ = BOTTOM_FREQ + (CHAN_SPACING * (NUM_CHANNELS-1)) / 2
 def channel_index(freq):
     """Return zero-based channel index for burst center frequency"""
     return round((freq - BOTTOM_FREQ) / CHAN_SPACING)
+
+
+def get_bottom_freq_hz(cfg_value):
+    """Convert 24-bit base frequency config value to Hz
+    e.g. 915 MHz band base frequency config value is 0x231B13, which corresponds to 912,749,725 Hz"""
+    f_osc = 26_000_000
+
+    x = cfg_value * f_osc
+
+    upper = cfg_value // 10825961
+    lower = (x >> 16) | ((x >> 32) * 0x10000)
+
+    return (upper << 32) | lower
